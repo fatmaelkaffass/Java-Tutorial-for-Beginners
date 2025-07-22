e004a6167680b0d30833607adca84d551c396ff3
fatmaelkaffass <fatmaelkaffass@gmail.com>
2025-07-22 16:21:14 +0300

    Static Objects Or Variables

---
+++ src/Encapsuation.java
@@ -0,0 +1,17 @@
+class Human
+{
+    int age ;
+    String name ;
+
+}
+
+public class Encapsuation {
+
+    public static void main(String[] args) {
+        Human obj = new Human();
+        obj.age = 11;
+        obj.name = "menna";
+         System.out.println(obj.name);
+    }
+
+}
